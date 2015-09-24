6a7bdd203acec9f529b74c4578044b8aeb8d197e
James Yeates <jimibue@gmail.com>
2015-09-24 11:03:44 -0600

    new files added

---
+++ async-patterns/callbacks.js
@@ -0,0 +1,7 @@
+/**
+ * Created by James Yeates on 9/24/15.
+ *
+ * callbacks are a continuation
+ *
+ * callback hell - Inversion of control - can makes us lose control of our code (third party libraries)
+ */
